1f238d42deb7c1e21aa3de8378b5e9b25e49f4ed
Suho-22300253 <22300253@handong.ac.kr>
2026-06-02 22:06:00 +0900

    Initial commit - Spring Boot project setup

---
+++ src/main/java/org/example/hgugaticar/HguGaticarApplication.java
@@ -0,0 +1,13 @@
+package org.example.hgugaticar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HguGaticarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HguGaticarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/hgugaticar/HguGaticarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hgugaticar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HguGaticarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
