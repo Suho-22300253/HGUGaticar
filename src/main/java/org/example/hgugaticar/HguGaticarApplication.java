3a46f37898749adeba1a98cde3a6af02c2de8d02
Suho-22300253 <22300253@handong.ac.kr>
2026-06-02 22:07:03 +0900

    Initial Spring Boot project setup with dependencies

--- src/main/java/org/example/hgugaticar/HguGaticarApplication.java
+++ src/main/java/org/example/hgugaticar/HguGaticarApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HguGaticarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HguGaticarApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HguGaticarApplication.class, args);
+    }
 
 }
